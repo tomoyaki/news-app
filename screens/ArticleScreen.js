b103745f7382ce1c1f6a8d1e90ef89e789ed025c
tomoyaki <tomoyaki74@gmail.com>
2021-09-15 16:47:44 +0900

    Add ClipScreen

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { WebView } from 'react-native-webview';
+import { useDispatch } from 'react-redux';
+import { addClip, deleteClip } from '../store/actions/user';
 
 export default function ArticleScreen({ route }) {
   const { article } = route.params;
+
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={styles.container}>
+      <TouchableOpacity
+        onPress={() => {
+          dispatch(addClip({ clip: article }));
+        }}
+      >
+        <Text style={{ margin: 10, fontSize: 30 }}>ADD_CLIP</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          dispatch(deleteClip({ clip: article }));
+        }}
+      >
+        <Text style={{ margin: 10, fontSize: 30 }}>DELETE_CLIP</Text>
+      </TouchableOpacity>
       <WebView source={{ uri: article.url }} />
     </SafeAreaView>
   );
