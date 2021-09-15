899371e2b2a8c09dc53d19408d48a2eb09dcfa7b
tomoyaki <tomoyaki74@gmail.com>
2021-09-15 17:26:43 +0900

    Implement redux-persist

--- screens/ClipScreen.js
+++ screens/ClipScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView, Text } from 'react-native';
+import { StyleSheet, SafeAreaView, Text, FlatList } from 'react-native';
+import { useSelector } from 'react-redux';
+import ListItem from '../components/ListItem';
 
 const styles = StyleSheet.create({
   container: {
@@ -8,10 +10,24 @@ const styles = StyleSheet.create({
   },
 });
 
-export default ClipScreen = () => {
+export default ClipScreen = ({ navigation }) => {
+  const user = useSelector((state) => state.user);
+  const { clips } = user;
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>ClipScreen</Text>
+      <FlatList
+        data={clips}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+            onPress={() => navigation.navigate('Article', { article: item })}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </SafeAreaView>
   );
 };
